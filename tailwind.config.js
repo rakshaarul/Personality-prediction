7681db5c32370d0acc84b4a08e183990818628ad
Raksha Arul <83902091+rakshaarul@users.noreply.github.com>
2023-07-30 21:52:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+              content: ["./src/**/*.{js,jsx,ts,tsx}"],
+              theme: {
+                extend: {
+                  animation: {
+                    'spin-slow': 'spin 100s linear infinite',
+                  }
+                },
+              },
+              plugins: [],
+            };
+        
